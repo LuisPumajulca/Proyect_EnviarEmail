034a4f19ae0ea8cc4033de54ef5a8a8d85e873cc
LuisPumajulca <pumajulca@hotmail.com>
2023-07-16 16:46:02 -0500

    Prevenir que se generen multiples alertas

--- js/app.js
+++ js/app.js
@@ -21,6 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function mostrarAlerta(mensaje, referencia) {
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta) {
+            alerta.remove();
+        }
+
         // Generando alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
